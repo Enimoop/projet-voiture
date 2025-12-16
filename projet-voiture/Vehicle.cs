510fdf11ad7b15ac4c1a51d4d233f5933101f3f3
erwanclx <erwanclou@gmail.com>
2025-12-16 13:10:10 +0100

    Add in model kilometers and optional dates | add functions to get need maintenance

--- projet-voiture/Vehicle.cs
+++ projet-voiture/Vehicle.cs
@@ -10,8 +10,23 @@ public class Vehicle
     public string Brand { get; set; }
     public string Model { get; set; }
     public int YearModel { get; set; }
+    public int BasicPrice { get; set; }
     public string Color { get; set; }
     public VehicleState State { get; set; }
+
+    public int KilometrageTotal { get; set; }
+    public int KilometrageActuel { get; set; }
+    public DateTime? LastMaintenance { get; set; }
+
+    public bool BesoinMaintenance()
+    {
+        if (LastMaintenance is not null)
+        {
+            return DateTime.Now - LastMaintenance > TimeSpan.FromDays(60);
+        }
+        return KilometrageActuel - KilometrageTotal > 10000;
+    }
+
 
     public void AfficherDetails()
     {
@@ -20,5 +35,6 @@ public class Vehicle
         Console.WriteLine($"Modèle: {Model}");
         Console.WriteLine($"Année: {YearModel}");
         Console.WriteLine($"Couleur: {Color}");
+        Console.WriteLine($"Besoin de Maintenance: {BesoinMaintenance()}");
     }
 }
