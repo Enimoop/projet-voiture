61204ac643d5958d3293abe35bd4cd975277d197
erwanclx <erwanclou@gmail.com>
2025-12-16 11:13:20 +0100

    Add enum for VehicleState

--- projet-voiture/Program.cs
+++ projet-voiture/Program.cs
@@ -1,10 +1,17 @@
 ﻿public class Vehicle
 {
+    public enum VehicleState
+    {
+        Available,
+        Rented,
+        Maintenance,
+    }
     public int Id { get; set; }
     public string Brand { get; set; }
     public string Model { get; set; }
     public int YearModel { get; set; }
     public string Color { get; set; }
+    public VehicleState State { get; set; }
 
     public void AfficherDetails()
     {
